966a51a4b12fe1d6334813c5755094b47acf9650
Simran Toor <simrankaurtoor@outlook.com>
2022-06-23 00:06:50 +0100

    mindfulness page complete

--- restful-app/src/Components/App/index.js
+++ restful-app/src/Components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import {useState, useReducer, useEffect} from 'react'
+import {/*useState,*/ useReducer, useEffect} from 'react'
 
 //import components
 import LandingPage from '../LandingPage'
--- restful-app/src/Components/Mindfulness/index.js
+++ restful-app/src/Components/Mindfulness/index.js
@@ -1,14 +1,17 @@
-import BurgerMenu from "../BurgerMenu"
-
+import React from "react";
+import BurgerMenu from "../BurgerMenu/index.js"
+import "./styles.css";
 const Mindfulness = ({handleClick})=> {
 
 
   return (
-    <div className='mindfulness-page'>
-      <h1>Mindfulness</h1>
+    <div className="mindfulness-page">
+      <h1 className="mindfulness-title">Mindfulness</h1>
 
       <BurgerMenu handleClick={handleClick}/>
-      
+      <div className="pulse">
+        
+      </div>
     </div>
   )
 
