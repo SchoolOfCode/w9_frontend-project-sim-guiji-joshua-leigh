88717e638af035e62664f677ceb9163d6ccfc7f0
Simran Toor <simrankaurtoor@outlook.com>
2022-06-23 01:28:30 +0100

    home page button now works, fixed menu alignment on home and mindfulness pages

--- restful-app/src/Components/HomePage/index.js
+++ restful-app/src/Components/HomePage/index.js
@@ -1,19 +1,21 @@
-import BurgerMenu from "../BurgerMenu";
+import React from "react";
+import BurgerMenu from "../BurgerMenu/index.js";
 import LavaLamp from '../LandingPage/LavalampBackground/LavalampBackground';
 import './styles.css';
 
 const HomePage = ({handleClick})=> {
-
-
   return (
-    <div className='home-page' >
-      <h1 className="title-home">HomePage</h1>
-      <LavaLamp />
+
+    <div className="home-page" >
       <BurgerMenu handleClick={handleClick}/>
+
+      <h1 className="title-home">HomePage</h1>
+
+
+      <LavaLamp />
       
     </div>
   )
-
 }
 
 export default HomePage
--- restful-app/src/Components/Mindfulness/index.js
+++ restful-app/src/Components/Mindfulness/index.js
@@ -1,20 +1,23 @@
 import React from "react";
 import BurgerMenu from "../BurgerMenu/index.js"
 import "./styles.css";
+
+
 const Mindfulness = ({handleClick})=> {
-
-
   return (
+
     <div className="mindfulness-page">
+      <BurgerMenu handleClick={handleClick}/>
+
       <h1 className="mindfulness-title">Mindfulness</h1>
 
-      <BurgerMenu handleClick={handleClick}/>
+
       <div className="pulse">
-        
+
       </div>
+
     </div>
   )
-
 }
 
 export default Mindfulness
