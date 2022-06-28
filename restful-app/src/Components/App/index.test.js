b1213f280ef56e43dcf3b4eb721ee0c9153a88c5
leigheastell <leigheastell1286@gmail.com>
2022-06-28 16:47:48 +0100

    wrote a test in App index.js can't get it to pass yet

--- restful-app/src/Components/App/index.test.js
+++ restful-app/src/Components/App/index.test.js
@@ -1,8 +1,18 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App/index.js';
 
+
+test('renders quote on LandingPage', () => {
+  render(<App />);
+  const quote = screen.getByText('Congratulations on making it to the School of Code, you should be really proud of yourself. We are really proud of you - The Restless Team');
+  expect(quote).toBeInTheDocument();
+});
+
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
